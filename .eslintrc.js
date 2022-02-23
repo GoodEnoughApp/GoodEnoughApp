d8f50acaecce3f9752da502fae4abba7e1af614c
Jose J. Cruz <jjzcru@gmail.com>
2022-02-23 00:22:24 -0500

    feat: :sparkles: Add screen for home

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
@@ -16,11 +16,10 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-one-expression-per-line": "off"
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-alert': 'off',
   },
 };
