4d48f9d0a7508a1bf26393b21c717ad1a594c060
Jose J. Cruz <jjzcru@gmail.com>
2022-02-24 00:08:14 -0500

    feat: :sparkles: Add view for products details

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgetPassword from './pages/ForgetPassword';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/signin" component={SignIn} />
             <Route exact path="/forget" component={ForgetPassword} />
+            <Route exact path="/products/:id" component={ProductDetails} />
             <Route exact path="/" component={Home} />
             <Redirect to="/" />
           </Switch>
