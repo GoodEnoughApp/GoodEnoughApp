093dece54e8b02d62203d804264a362a66006e5c
Jose J. Cruz <jjzcru@gmail.com>
2022-03-23 18:55:21 -0400

    feat: :sparkles: Add support for change password

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -10,6 +10,7 @@ const validNames = [
   'chevron-left',
   'share',
   'user',
+  'lock',
 ];
 export default function Icon({ name, color }) {
   switch (name) {
@@ -29,6 +30,8 @@ export default function Icon({ name, color }) {
       return <Share color={color} />;
     case 'user':
       return <User color={color} />;
+    case 'lock':
+      return <Lock color={color} />;
     default:
       return null;
   }
@@ -201,6 +204,26 @@ function User({ color }) {
     >
       <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
       <circle cx="12" cy="7" r="4" />
+    </svg>
+  );
+}
+
+function Lock({ color }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-lock"
+    >
+      <rect x="3" y="11" width="18" height="11" rx="2" ry="2" />
+      <path d="M7 11V7a5 5 0 0 1 10 0v4" />
     </svg>
   );
 }
