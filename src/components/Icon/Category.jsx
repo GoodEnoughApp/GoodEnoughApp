cd746ab7c4063e25eef5d81a89be456d71893043
Jose J. Cruz <jjzcru@gmail.com>
2022-04-20 19:20:29 -0400

    feat: :sparkles: Add support for seafood

--- src/components/Icon/Category.jsx
+++ src/components/Icon/Category.jsx
@@ -25,6 +25,7 @@ const validNames = [
   'grains_cereals',
   'desserts_sweet_snacks',
   'dairy_free_meat_substitutes',
+  'seafood_seaweed',
 ];
 export function CategoryIcon({ name }) {
   switch (name) {
@@ -72,6 +73,8 @@ export function CategoryIcon({ name }) {
       return <Desserts />;
     case 'dairy_free_meat_substitutes':
       return <Substitutes />;
+    case 'seafood_seaweed':
+      return <Seafood />;
     default:
       return <Food />;
   }
@@ -1014,7 +1017,6 @@ function Grain() {
     <svg
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
       x="0px"
       y="0px"
       viewBox="0 0 54.081 54.081"
@@ -1240,6 +1242,53 @@ function Substitutes() {
           />
         </g>
       </g>
+    </svg>
+  );
+}
+
+function Seafood() {
+  const st0 = {
+    fill: 'none',
+    stroke: '#000000',
+    strokeWidth: 2,
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    strokeMiterlimit: 10,
+  };
+  return (
+    <svg
+      version="1.1"
+      id="Icons"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 32 32"
+      style={{ enableBackground: 'new 0 0 32 32' }}
+      xmlSpace="preserve"
+    >
+      <path
+        style={st0}
+        d="M28,9L28,9c0-2.2-1.8-4-4-4h-7.2C15,5,13.3,4.3,12,3l0,0"
+      />
+      <path
+        style={st0}
+        d="M28,9c0,5-4,9-9,9l-6.4,0c-1.3,0-2.6,0.9-2.6,2.2c0,1.8,1.5,2.8,3,2.8"
+      />
+      <line style={st0} x1="22" y1="11" x2="22" y2="13" />
+      <path
+        style={st0}
+        d="M8.8,10.4C8.2,11.3,8,12.3,8,13.5c0,2.2,1.1,4.1,2.7,5.3"
+      />
+      <path
+        style={st0}
+        d="M28,9L13.9,9c-4.3,0-8.5,2.5-9.5,6.7C3,22,7.4,27,13,27"
+      />
+      <path
+        style={st0}
+        d="M13,23v4l0.8-0.3c1.4-0.5,2.9-0.2,4.1,0.7l0,0c1.5,1.2,3.8,0.6,4.4-1.3l0,0c0.4-1.3-0.1-2.6-1.2-3.3l0,0
+	c-1.9-1.2-4.3-1.3-6.4-0.5L13,23z"
+      />
     </svg>
   );
 }
