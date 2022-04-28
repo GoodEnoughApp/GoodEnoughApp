8e03a4e9011a8fd456f55773ce151ef357654f0e
Jose J. Cruz <jjzcru@gmail.com>
2022-04-27 23:13:01 -0400

    feat: :sparkles: Add support for date range picker

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,42 +1,39 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+import { DayPickerRangeController } from 'react-dates';
 import { useState, useEffect, createContext, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-// import moment from 'moment';
+
+import moment from 'moment';
 
 import AppContext from '../AppContext';
 import Navbar from '../components/Navbar';
-import Icon from '../components/Icon';
+// import Icon from '../components/Icon';
 import Loading from '../components/Loading';
 import Profile from '../components/Profile';
-import UpdateShoppingItem from '../components/UpdateShoppingItem';
-import { Empty as Whale } from '../components/Empty';
 import { useHealth } from '../hooks/health';
-import useShopping from '../hooks/useShopping';
 
 import styles from './Base.module.css';
+import 'react-dates/lib/css/_datepicker.css';
 
 const ViewContext = createContext({});
 
 export default function Shopping() {
   const history = useHistory();
+  const [startDate, setStartDate] = useState(moment().startOf('week'));
+  const [endDate, setEndDate] = useState(moment().endOf('week'));
+  const [focusedInput, setFocusedInput] = useState('startDate');
   const { isOnline } = useHealth();
   const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const isLoading = false;
   const { api } = useContext(AppContext);
-  const { isLoading, categories, products, items, onRemoveItem, onUpdateItem } =
-    useShopping(api);
-  const [selection, setSelection] = useState(null);
   const classNames = [styles.page];
 
   useEffect(() => {
     if (!localStorage.getItem('token')) {
       history.replace('/signin');
     }
-  }, [isLoading, isOnline]);
-
-  if (selection) {
-    classNames.push(styles.selected);
-  }
+  }, [isOnline]);
 
   const me = JSON.parse(localStorage.getItem('me') || '{}');
 
@@ -44,14 +41,13 @@ export default function Shopping() {
     <ViewContext.Provider
       value={{
         api,
-        items,
         isOnline,
-        onUpdateItem,
-        onRemoveItem,
-        selection,
-        setSelection,
-        products,
-        categories,
+        startDate,
+        setStartDate,
+        endDate,
+        setEndDate,
+        focusedInput,
+        setFocusedInput,
       }}
     >
       <div className={classNames.join(' ')}>
@@ -62,7 +58,7 @@ export default function Shopping() {
           selected="report"
         />
         <Content isLoading={isLoading} />
-        <Selection selection={selection} />
+        <Selection />
         <Profile
           me={me}
           api={api}
@@ -85,7 +81,14 @@ export default function Shopping() {
 }
 
 function Content({ isLoading }) {
-  const { items } = useContext(ViewContext);
+  const {
+    startDate,
+    setStartDate,
+    endDate,
+    setEndDate,
+    focusedInput,
+    setFocusedInput,
+  } = useContext(ViewContext);
   if (isLoading) {
     return (
       <section>
@@ -94,172 +97,40 @@ function Content({ isLoading }) {
     );
   }
 
-  if (!items.length) {
-    return (
-      <section>
-        <Empty />
-      </section>
-    );
-  }
-
   return (
     <section className={[styles.content, styles['layout-2']].join(' ')}>
       <div className={styles.topbar}>
         <div />
-        <div>Shopping List</div>
-        <div>{items.length ? <ShareButton /> : null}</div>
+        <div>Report</div>
+        <div />
       </div>
-      <div className={styles.list}>
-        {items.map((item) => (
-          <ShoppingItem key={item.id} item={item} />
-        ))}
+      <div className={[styles.list, styles.report].join(' ')}>
+        <div className={styles['date-range-picker']}>
+          <DayPickerRangeController
+            startDate={startDate}
+            focusedInput={focusedInput}
+            endDate={endDate}
+            numberOfMonths={1}
+            onFocusChange={(change) => {
+              if (change === null) {
+                setFocusedInput('startDate');
+                return;
+              }
+              setFocusedInput(change);
+            }}
+            onDatesChange={(response) => {
+              setStartDate(response.startDate);
+              setEndDate(response.endDate);
+            }}
+          />
+        </div>
       </div>
     </section>
   );
 }
 
-function ShareButton() {
-  const { items } = useContext(ViewContext);
-  const values = [
-    'Shopping List',
-    '------------',
-    ...items.map((item) => {
-      let content = item.title;
-      if (item.quantity > 1) {
-        content = `${content} x ${item.quantity}`;
-      }
-
-      return `- [ ] ${content}`;
-    }),
-  ];
-  const text = values.join('\n');
-  let onClick = () => {};
-
-  if (navigator.clipboard) {
-    onClick = () => {
-      navigator.clipboard.writeText(text).then(
-        () => {
-          alert('Shopping list is in the clipboard');
-        },
-        (err) => {
-          alert(err.message);
-        },
-      );
-    };
-  }
-
-  if (navigator.share) {
-    onClick = () => {
-      navigator.share({
-        title: 'Shopping List',
-        text,
-      });
-    };
-  }
-  return (
-    <button type="button" onClick={onClick}>
-      <Icon name="share" />
-    </button>
-  );
-}
-
-function Empty() {
-  return <Whale type="whale" />;
-}
-
-function Selection({ selection }) {
-  const { api } = useContext(AppContext);
-  const { setSelection, isLoading, onRemoveItem, onUpdateItem, isOnline } =
-    useContext(ViewContext);
-  if (!selection || isLoading) {
-    return <Unselect />;
-  }
-
-  return (
-    <UpdateShoppingItem
-      api={api}
-      item={selection}
-      isOnline={isOnline}
-      goBack={() => {
-        setSelection(null);
-      }}
-      onUpdate={(item) => {
-        onUpdateItem(item);
-        setSelection(null);
-      }}
-      onDelete={(item) => {
-        onRemoveItem(item);
-        setSelection(null);
-      }}
-    />
-  );
-}
-
-function ShoppingItem({ item }) {
-  const { api, setSelection, selection, onRemoveItem, isOnline } =
-    useContext(ViewContext);
-  const { id, title, quantity, cost } = item;
-
-  const [isLoading, setIsLoading] = useState(false);
-  const onClickChecked = async () => {
-    if (
-      !confirm(
-        `Are you sure you want to remove "${title}" from the shopping list?`,
-      )
-    ) {
-      return;
-    }
-    setIsLoading(true);
-    api
-      .deleteShoppingItem({ id })
-      .then(({ status }) => {
-        setIsLoading(false);
-        if (status !== 'success') {
-          alert('An error happened removing the item');
-          return;
-        }
-        onRemoveItem({ id });
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        alert(err.message);
-      });
-  };
-  const classNames = [styles['shopping-item']];
-  if (selection && selection.id === item.id) {
-    classNames.push(styles.selected);
-  }
-
-  return (
-    <div id={id} className={classNames.join(' ')}>
-      <div className={styles.image}>
-        <img alt="product" />
-      </div>
-      <div
-        role="button"
-        onClick={() => {
-          setSelection(item);
-        }}
-        className={styles.content}
-      >
-        <div>
-          <h5>
-            {title} {quantity > 1 ? <em>(x{quantity})</em> : null}
-          </h5>
-          <small>${parseFloat(`${quantity * cost}`).toFixed(2)}</small>
-        </div>
-      </div>
-      <div className={styles.actions}>
-        <button
-          type="button"
-          disabled={isLoading || !isOnline}
-          onClick={onClickChecked}
-        >
-          <Icon name="check-square" />
-        </button>
-      </div>
-    </div>
-  );
+function Selection() {
+  return <Unselect />;
 }
 
 function Unselect() {
